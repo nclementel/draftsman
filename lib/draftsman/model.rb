a5d33906246eeb7467e6046d962edead5538d7d2
Nicola Clementel <nicola@vertical-life.info>
2019-06-12 12:13:35 +0200

    test

--- lib/draftsman/model.rb
+++ lib/draftsman/model.rb
@@ -412,7 +412,9 @@ module Draftsman
             if self.draft? && self.draft.changeset && self.draft.changeset.key?(attr)
               the_changes[attr] = [self.draft.changeset[attr].first, send(attr)]
             else
+              puts attr
               puts self.send("#{attr}_was")
+              puts send(attr)
               the_changes[attr] = [self.send("#{attr}_was"), send(attr)]
             end
           end
