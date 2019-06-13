843ad0ea5a9579caae1cfba1d7116e49213ae21b
Nicola Clementel <nicola@vertical-life.info>
2019-06-13 13:23:58 +0200

    test

--- lib/draftsman/model.rb
+++ lib/draftsman/model.rb
@@ -411,7 +411,7 @@ module Draftsman
         only   = self.class.draftsman_options[:only]
         draftable_attrs = self.attributes.keys - ignore - skip
         draftable_attrs = draftable_attrs & only if only.present?
-        draftable_attrs.delete_if { |k| object.translated_attribute_names.include? k }
+        draftable_attrs.delete_if { |k| self.translated_attribute_names.include? k }
 
         # If there's already an update draft, get its changes and reconcile them
         # manually.
