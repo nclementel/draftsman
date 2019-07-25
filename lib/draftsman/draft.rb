8294aeb77cd1f01b71bfecdc58ee0f49bacf6738
Nicola Clementel <nicola@vertical-life.info>
2019-07-25 11:52:23 +0200

    test

--- lib/draftsman/draft.rb
+++ lib/draftsman/draft.rb
@@ -181,7 +181,11 @@ class Draftsman::Draft < ActiveRecord::Base
         # Write `published_at` attribute
         self.item.send("#{self.item.class.published_at_attribute_name}=", current_time_from_proper_timezone)
 
+        # Clear out draft
+        self.item.send("#{self.item.class.draft_association_name}_id=", nil)
+
         self.item.save(self.item.draftsman_options[:publish_options].merge(options))
+        self.item.reload
 
         self.item.attributes = self.reify.attributes if Draftsman.stash_drafted_changes? && self.update?
 
@@ -190,11 +194,6 @@ class Draftsman::Draft < ActiveRecord::Base
             l.update("#{attr}"=> l.send(attr))
           end
         end
-
-        # # Clear out draft
-        # self.item.send("#{self.item.class.draft_association_name}_id=", nil)
-        # self.item.save(self.item.draftsman_options[:publish_options].merge(options))
-        self.item.reload
 
         # Destroy draft
         self.destroy
